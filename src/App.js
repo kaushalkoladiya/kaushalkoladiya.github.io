6033bd1e840a451b2d0194e9cdecefa0b792f5e6
Kaushal <50080954+kausHal789@users.noreply.github.com>
2020-04-18 16:15:13 +0530

    1.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Container, Row, Col, Image } from 'react-bootstrap';
 
 import './App.css';
 import Avtar from './images/avtar.JPG';
@@ -29,37 +28,37 @@ class App extends React.Component {
           <div className="container">
             <div className="row">
               <div className="col-lg-8 mx-auto">
-                <h2 class="text-white mb-4">Hii, I'm Kaushal Koladiya</h2>
-                <p class="text-white-50">
+                <h2 className="text-white mb-4">Hii, I'm Kaushal Koladiya</h2>
+                <p className="text-white-50">
                   Hello, I'm a student, self-learner, developer and Javascript lover.<br />
                   The JS guy.<br/>
                   Currently exploring a React.js and React Native.
                 </p>
                 <h5 className="text-white-50 font-weight-bold">Skills</h5>
-                <p>
+                <div className="p">
                   <span className="text-white-50">Programming languages</span>
-                  <div class="progress my-2"><div class="progress-bar bg-warning progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Javascript</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-info progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>C/C++</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-warning progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Python</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-primary progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "1000%"}}>PHP</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-success progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "1000%"}}>JAVA</div></div>
-                </p>
-                <p>
+                  <div className="progress my-2"><div className="progress-bar bg-warning progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Javascript</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-info progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>C/C++</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-warning progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Python</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-primary progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "1000%"}}>PHP</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-success progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "1000%"}}>JAVA</div></div>
+                </div>
+                <div className="p">
                   <span className="text-white-50">Databases(SQL & NOSQL)</span>
-                  <div class="progress my-2"><div class="progress-bar bg-primary progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>MySQL</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-dark progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>SQLite</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-success progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>MongoDB</div></div>
-                </p>
-                <p>
+                  <div className="progress my-2"><div className="progress-bar bg-primary progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>MySQL</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-dark progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>SQLite</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-success progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>MongoDB</div></div>
+                </div>
+                <div className="p">
                   <span className="text-white-50">Framworks & others</span>
-                  <div class="progress my-2"><div class="progress-bar bg-primary progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>React.js</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-warning progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Node.js</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-success progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Express js</div></div>
-                  <div class="progress my-2"><div class="progress-bar bg-danger progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Laravel</div></div>
-                </p>
+                  <div className="progress my-2"><div className="progress-bar bg-primary progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>React.js</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-warning progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Node.js</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-success progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Express js</div></div>
+                  <div className="progress my-2"><div className="progress-bar bg-danger progress-bar-striped sm progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: "100%"}}>Laravel</div></div>
+                </div>
               </div>  
             </div>
-            <img src={Avtar} class="img-fluid rounded-circle img-thumbnail" alt="avatar" />
+            <img src={Avtar} className="img-fluid rounded-circle img-thumbnail" alt="avatar" />
           </div>
         </section>
 
@@ -72,50 +71,93 @@ class App extends React.Component {
               </div>
               <div className="col-xl-4 col-lg-5">
                 <div className="featured-text text-center text-lg-left">
-                  <h4>Shoreline</h4>
-                  <p className="text-black-50 mb-0">My projects are briskly and scalable.</p>
+                  <h4>Headline</h4>
+                  <p className="text-black-50 mb-0">Let's walk through my projects and blogs.</p>
+                </div>
+              </div>
+            </div>
+            
+            <div className="row m-3"> 
+              <div className="col display-4 text-center d-block">
+                Projects
+              </div>
+            </div>
+
+            <div className="row m-2 justify-content-center">
+              <div className="card shadow m-2" style={{width: "18rem"}}>
+                <div className="card-body">
+                  <h5 className="card-title">Corona Dashboard</h5>
+                  <p className="card-text">A graphical representation of Covid-19 Dashboard.</p>
+                  <a href="https://github.com/kausHal789/covid-19" className="card-link">GitHub Link</a>
+                  <a href="https://coronadashboard.netlify.com" className="card-link">Go for website</a>
+                </div>
+              </div>
+              <div className="card m-2 shadow" style={{width: "18rem"}}>
+                <div className="card-body">
+                  <h5 className="card-title">News</h5>
+                  <p className="card-text">A latest news headlines.</p>
+                  <a href="https://github.com/kausHal789/corona-news" className="card-link">GitHub Link</a>
+                  <a href="https://headlines.netlify.com" className="card-link">Go for website</a>
                 </div>
               </div>
             </div>
 
-            <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
-              <div className="col-lg-6">
-                <img className="img-fluid" src="img/demo-image-01.jpg" alt="" />
-              </div>
-              <div className="col-lg-6">
-                <div class="bg-black text-center h-100 project">
-                  <div class="d-flex h-100">
-                    <div class="project-text w-100 my-auto text-center text-lg-left">
-                      <h4 class="text-white">Misty</h4>
-                      <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
-                      <hr class="d-none d-lg-block mb-0 ml-0" />
-                    </div>
-                  </div>
-                </div>
+            <div className="row m-3"> 
+              <div className="col display-4 text-center d-block">
+                Blogs
               </div>
             </div>
 
-            <div class="row justify-content-center no-gutters">
-              <div class="col-lg-6">
-                <img class="img-fluid" src="img/demo-image-02.jpg" alt="" />
+            <div className="row justify-content-center">
+              <div className="card shadow-lg p-2 m-2">
+                <div className="card-header border-0 rounded">
+                  Javascript Arrow Function
+                </div>
+                <div className="card-body">
+                  <blockquote className="blockquote mb-0">
+                    <p>ES6 comes with a new function syntax which looks very different from others...</p>
+                    <footer className="blockquote-footer">Kaushal Koladiya <a href="https://dev.to/kaushal789/javascript-arrow-function-2513" className="btn btn-link text-primary">Read more</a></footer>
+                  </blockquote>
+                </div>
               </div>
-              <div class="col-lg-6 order-lg-first">
-                <div class="bg-black text-center h-100 project">
-                  <div class="d-flex h-100">
-                    <div class="project-text w-100 my-auto text-center text-lg-right">
-                      <h4 class="text-white">Mountains</h4>
-                      <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
-                      <hr class="d-none d-lg-block mb-0 mr-0" />
-                    </div>
-                  </div>
+              <div className="card shadow-lg p-2 m-2">
+                <div className="card-header border-0 rounded">JavaScript destructuring</div>
+                <div className="card-body">
+                  <blockquote className="blockquote mb-0">
+                    <p>JavaScript ES6 comes with the destructuring assignments. A name define its use, destructure values from an array and properties from an object..</p>
+                    <footer className="blockquote-footer">Kaushal Koladiya <a href="https://dev.to/kaushal789/javascript-destructuring-1b4d" className="btn btn-link text-primary">Read more</a></footer>
+                  </blockquote>
                 </div>
               </div>
             </div>
-
           </div>
         </section>
 
-       
+        <footer>
+          <div className="row">
+            <div className="col font-weight-bold small text-black-50 my-1 d-block text-center">
+              Catch me on!
+            </div>
+          </div>
+          <div className="container d-flex justify-content-center">
+            {/* <a href="https://twitter.com/kaushal__p" className="mx-2">
+              <i className="fa fa-lg fa-tag fa-twitter"></i>
+            </a> */}
+            <a href="https://www.linkedin.com/in/kaushal-koladiya-a65676179" className="mx-2">
+              <i className="fa fa-lg fa-tag fa-linkedin"></i>
+            </a>
+            {/* <a href="https://instagram.com/kaushalkoladiya" className="mx-2">
+              <i className="fa fa-lg fa-tag fa-instagram"></i>
+            </a>
+            <a href="https://www.facebook.com/kaushal.koladiya.1" className="mx-2">
+              <i className="fa fa-lg fa-facebook"></i>
+            </a> */}
+            <a href="https://github.com/kausHal789" className="mx-2">
+              <i className="fa fa-lg fa-github"></i>
+            </a>
+          </div>
+        </footer>
+
       </div>
     );
   }
